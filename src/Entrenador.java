b249c4e2929488cf3264e15e434047158c7816f6
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-05-22 11:45:25 -0600

    first commit

---
+++ src/Entrenador.java
@@ -0,0 +1,20 @@
+public class Entrenador {
+
+    private String idFederación;
+
+    public Entrenador() {
+    }
+
+    public Entrenador(String idFederación) {
+        this.idFederación = idFederación;
+    }
+
+    public String getIdFederación() {
+        return idFederación;
+    }
+
+    public void setIdFederación(String idFederación) {
+        this.idFederación = idFederación;
+    }
+    
+}
